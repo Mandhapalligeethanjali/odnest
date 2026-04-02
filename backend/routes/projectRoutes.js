3966fcb04fcc3fef5cd3ad91b26b831d33d95c9f
M Geetha <mandhapalligeethanjali@gmail.com>
2026-04-03 01:23:30 +0530

    feat: add project API - create, read, update, delete, filter projects

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
-router.get('/', (req, res) => res.json({ message: 'Project routes working' }));
+const {
+  createProject,
+  getAllProjects,
+  getProjectById,
+  updateProject,
+  deleteProject,
+  getMyProjects
+} = require('../controllers/projectController');
+const { protect, authorize } = require('../middleware/authMiddleware');
+
+// Public routes
+router.get('/',              getAllProjects);
+router.get('/:id',           getProjectById);
+
+// Private routes
+router.post('/',             protect, authorize('client'), createProject);
+router.put('/:id',           protect, authorize('client'), updateProject);
+router.delete('/:id',        protect, authorize('client'), deleteProject);
+router.get('/my/projects',   protect, getMyProjects);
+
 module.exports = router;
